d2c716be8279285088b1ead279c0d8043ad4f0fc
Lilyokalani Hafoka <lilyhafoka.du@gmail.com>
2021-02-06 14:37:52 -0700

    another deploy

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -22,7 +22,7 @@ router.post('/api/workouts', (req, res) => {
 
 router.put('/api/workouts/:id', ({body, params}, res) => {
     Workout.findByIdAndUpdate(params.id, 
-        {$push: {exercises: body}},
+        {$push: {exercise: body}},
         {new: true, runValidators: true}
     ).then(data => {
         res.json(data)
